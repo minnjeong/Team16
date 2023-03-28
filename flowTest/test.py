043b51d56e0946112abe4f289d192771857f7e45
vegatality <vegatality@gmail.com>
2023-03-29 00:09:46 +0900

    흐름 확인용, 테스트 용도(주의!! 몽고DB 로그인 부분 자기 데이터베이스에 맞게 변경한 후 사용할 것.)

---
+++ flowTest/test.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import pyautogui
+import time
+from pymongo import MongoClient
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+client = MongoClient("mongodb+srv://sparta16:sparta16@cluster16.b0dkofq.mongodb.net/?retryWrites=true&w=majority")
+db = client.movie
+
+@app.route('/')
+def home():
+    return render_template('test.html')
+
+@app.route("/movie/flag", methods=["GET"])
+def flag_check():
+    # flag 값 가져오기.(맨 처음에 비어있으면 True로 값 넣어줌. 이후부터는 False로 바꿔줌.)
+    flagList = list(db.flag.find({},{"_id":False}))
+    if flagList == None or len(flagList)==0 :
+        db.flag.insert_one({"flag":True})
+        flagList = list(db.flag.find({},{"_id":False}))
+    else:
+        flagList = list(db.flag.find({},{"_id":False}))
+    flag = flagList[-1]
+    print(flag)  ## {'flag': True}
+    ## 여기 밑에서  {"flag":{'flag': True}} 이렇게 넘어감.
+    return jsonify({ "flag": flag })
+    
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    ## 다시 플래그 상태 바꿔서 영화 데이터 더 이상 못 불러오게 하기.
+    db.flag.update_one({"flag": True}, {"$set":{"flag": False}})
+    flagList = list(db.flag.find({},{"_id":False}))
+    flag = flagList[-1]
+    print(flag) ## {'flag': False}
+    return jsonify({"flag": flag})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
