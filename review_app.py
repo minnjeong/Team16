7d8f353e282821db90dc368c281104ad7f4ff388
minnjeong <124552434+minnjeong@users.noreply.github.com>
2023-03-30 18:19:12 +0900

    Update review_app.py

--- review_app.py
+++ review_app.py
@@ -8,14 +8,6 @@ db = client.dbsparta
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/bi/mi/point.naver?code=217747&onlyActualPointYn=Y#pointAfterTab',headers=headers)
-
-soup = BeautifulSoup(data.text, 'html.parser')
-
-a = soup.select_one('#content > div.article > div.mv_info_area > div.mv_info > h3 > a:nth-child(1)').text
-print(a)
-
 # ----------------------
 
 @app.route('/')
